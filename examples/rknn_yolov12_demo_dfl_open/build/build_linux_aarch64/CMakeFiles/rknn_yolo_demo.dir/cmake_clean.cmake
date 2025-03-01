743ae344827384f80fcbe6fcbdb499aa93b32297
cqu20160901 <1013429609@qq.com>
2025-03-01 22:51:51 +0800

    upload

---
+++ examples/rknn_yolov12_demo_dfl_open/build/build_linux_aarch64/CMakeFiles/rknn_yolo_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rknn_yolo_demo.dir/src/main.cc.o"
+  "CMakeFiles/rknn_yolo_demo.dir/src/postprocess.cc.o"
+  "rknn_yolo_demo"
+  "rknn_yolo_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rknn_yolo_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
